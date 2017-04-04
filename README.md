bba46a1c9ff103dfc64bc758b403dec927d2ab61
kizniche <KyleTGabriel@gmail.com>
2017-04-03 23:53:20 -0400

    Refactor to detect image similarity (to exclude duplicate frames) and permit multiple GIF creation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# longer-weather
-Combines animated radar images to create a longer animation.
+# Longer Weather
+
+This software downloads short GIFs at predefined intervals, extracts the frames, and combines them into a longer GIF. It is useful for automatically generating longer GIFs from short GIFs that are updated, such as weather radar animations. Two weather radar GIFs from intellicast.net are used by default, but it should work with any GIF.
--- longer_weather.py
+++ longer_weather.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-#  longer_weather.py - Collect radar GIFs from intellicast.net and merge them together
+#  longer_weather.py - Collect radar GIFs from intellicast.net and
+#                      merge them together
 #
 #  Copyright (C) 2015  Kyle T. Gabriel
 #
@@ -19,140 +20,227 @@
 #  along with Mycodo. If not, see <http://www.gnu.org/licenses/>.
 #
 #  Contact at kylegabriel.com
-
-
-#### Configure Directories ####
-install_directory = "/home/user/longer-weather"
-
-import getopt
+#
+#
+#  Prerequisites: pillow tesseract imagehash imageio
+#  8 new frames are generated roughly every 2 hours 15 minutes
+#
+import argparse
 import logging
+import grp
+import imagehash
+import imageio
 import os
-import subprocess
+import pwd
 import sys
 import time
 import urllib
-from datetime import datetime, timedelta
-from threading import Timer
+from PIL import Image
+from datetime import datetime
+from datetime import timedelta
+from shutil import copyfile
+from threading import Thread
 
-image_number = 0
+REF_PERIOD_MIN = 16
+GIF_HTTP_FILES = [
+    'http://images.intellicast.com/WxImages/RadarLoop/usa_None_anim.gif',
+    'http://images.intellicast.com/WxImages/RadarLoop/csg_None_anim.gif'
+]
 
-log_path = "%s/log" % install_directory
-daemon_log_file_tmp = "%s/daemon-tmp.log" % log_path
+INSTALL_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+LOG_PATH = os.path.join(INSTALL_DIRECTORY, 'status.log')
+COMB_PATH = os.path.join(INSTALL_DIRECTORY, 'combined')
+FRAME_PATH = os.path.join(INSTALL_DIRECTORY, 'frames')
 
-logging.basicConfig(
-    filename = daemon_log_file_tmp,
-    level = logging.INFO,
-    format = '%(asctime)s [%(levelname)s] %(message)s')
-    
-logging.getLogger().setLevel(logging.INFO)
+logFormatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+logger = logging.getLogger()
 
-console = logging.StreamHandler()
-console.setLevel(logging.INFO)
-# add the handler to the root logger
-logging.getLogger('').addHandler(console)
+fileHandler = logging.FileHandler(LOG_PATH)
+fileHandler.setFormatter(logFormatter)
+logger.addHandler(fileHandler)
 
-#filename = link.split('/')[-1]
-# Displays the program usage
-def usage():
-    print "lweather.py: Collect radar GIFs from intellicast.net and merge them together" \
-        "\n"
-    print "Usage:  lweather.py [OPTION]...\n"
-    print "Options:"
-    print "    -g, --generate"
-    print "           Only generate animation from gifs in main directory"
-    print "    -s, --sync Minutes [Start Number]"
-    print "           Synchronize 2-hour intervals based on a 24-hour clock"
-    print "           Enter only two-digits for Minutes (0-60)"
-    print "           Optional: Enter a Start Number for file names"
-    print "    -h, --help"
-    print "           Display this help and exit\n"
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+logger.addHandler(consoleHandler)
 
-# Check user options and arguments for validity
-def menu():
-    global image_number
-    
-    if len(sys.argv) == 1: # No arguments given
-        daemon(0)
-        sys.exit(1)
+logger.setLevel(logging.INFO)
 
+
+class LongerWeather(Thread):
+    """Longer Weather Class"""
+    def __init__(self):
+        Thread.__init__(self)
+        self.frame_number = {}
+        self.last_img_sizes = {}
+        for each_gif in GIF_HTTP_FILES:
+            file_name = each_gif.split('/')[-1][:-4]
+            self.frame_number[file_name] = 1
+            self.last_img_sizes[file_name] = []
+        self.start_time = datetime.now()
+        self.timer = datetime.now()
+        self.running = True
+
+    def run(self):
+        logger.info("[Daemon] Daemon Started")
+
+        while self.running:
+            present = datetime.now()
+            if present > self.timer:
+                self.timer = self.timer + timedelta(minutes=REF_PERIOD_MIN)
+                for each_gif in GIF_HTTP_FILES:
+                    gif_name = each_gif.split('/')[-1][:-4]
+                    logger.info("[Daemon] Saving GIF from intellicast.net")
+                    gif_image_file = self.get_gif(each_gif)
+                    logger.info("[Daemon] GIF acquired, extracting frames")
+                    self.process_image(gif_image_file, gif_name)
+                    logger.info("[Daemon] Deleting GIF")
+                    os.remove(gif_image_file)
+                    logger.info("[Daemon] Creating combined GIF")
+                    self.combine_gif(gif_name)
+                logger.info("[Daemon] Next Grab: {next:.2f} minutes".format(
+                    next=(self.timer - present).total_seconds() / 60))
+            time.sleep(1)
+
+    @staticmethod
+    def get_gif(gif_address):
+        date_now = datetime.now().strftime("%Y_%m_%d_%H-%M-%S")
+        gif_image_file = "{path}/frames/{date}-{name}.gif".format(
+            path=INSTALL_DIRECTORY, date=date_now, name=gif_address.split('/')[-1][:-4])
+        urllib.urlretrieve(gif_address, gif_image_file)
+        return gif_image_file
+
+    def combine_gif(self, gif_name):
+        save_path = '{path}/combined/{date}-{name}.gif'.format(
+            path=INSTALL_DIRECTORY,
+            date=self.start_time.strftime("%Y_%m_%d_%H-%M-%S"),
+            name=gif_name)
+        images = []
+        for dir_name, _, file_names in os.walk(FRAME_PATH):
+            for each_name in sorted(file_names):
+                if gif_name in each_name:
+                    images.append(imageio.imread(os.path.join(dir_name, each_name)))
+        imageio.mimsave(save_path, images)
+
+    def terminate(self):
+        self.running = False
+
+    @staticmethod
+    def analyze_image(path):
+        """
+        Pre-process pass over the image to determine the mode (full or additive).
+        Necessary as assessing single frames isn't reliable. Need to know the mode 
+        before processing all frames.
+        """
+        im = Image.open(path)
+        results = {
+            'size': im.size,
+            'mode': 'full',
+        }
+        try:
+            while True:
+                if im.tile:
+                    tile = im.tile[0]
+                    update_region = tile[1]
+                    update_region_dimensions = update_region[2:]
+                    if update_region_dimensions != im.size:
+                        results['mode'] = 'partial'
+                        break
+                im.seek(im.tell() + 1)
+        except EOFError:
+            pass
+        return results
+
+    def process_image(self, img_path, gif_name):
+        """ Iterate the GIF, extracting each frame """
+        mode = self.analyze_image(img_path)['mode']
+        im = Image.open(img_path)
+        i = 0
+        p = im.getpalette()
+
+        try:
+            while True:
+                logger.debug('saving {path} ({mode}) frame {frame}, {size} {title}'.format(
+                    path=img_path, mode=mode, frame=i, size=im.size, title=im.tile))
+
+                # If the GIF uses local colour tables, each frame will have its own palette
+                # If not, we need to apply the global palette to the new frame
+                if not im.getpalette():
+                    im.putpalette(p)
+
+                new_frame = Image.new('RGBA', im.size)
+                new_frame.paste(im, (0, 0), im.convert('RGBA'))
+
+                # Save frame to file
+                check_frame_path = '{path}/check_frame.png'.format(path=FRAME_PATH)
+                new_frame.save(check_frame_path, 'PNG')
+
+                # hash frame image and compare hash to last hashed frames.
+                # If hash is unique, it's a new frame. Save it as a new file.
+                img_hash = imagehash.dhash(Image.open(check_frame_path), hash_size=12)
+                if str(img_hash) not in self.last_img_sizes[gif_name]:
+                    self.last_img_sizes[gif_name].append(str(img_hash))
+                    logger.info("Hashes: {}".format(self.last_img_sizes))
+                    frame_name = '{name}-{frame:0>5}.png'.format(
+                        name=gif_name, frame=self.frame_number[gif_name])
+                    new_file = os.path.join(FRAME_PATH, frame_name)
+                    copyfile(check_frame_path, new_file)
+                    self.frame_number[gif_name] += 1
+
+                os.remove(check_frame_path)
+                i += 1
+                im.seek(im.tell() + 1)
+
+                # Ensure the list stays relatively small
+                if len(self.last_img_sizes[gif_name]) > 16:
+                    self.last_img_sizes[gif_name].pop(0)
+        except EOFError:
+            pass
+
+
+def assure_path_exists(dir_path):
+    """ Create path if it doesn't exist """
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+        os.chmod(dir_path, 0774)
+        # set_user_grp(dir_path, 'mycodo', 'mycodo')
+    return dir_path
+
+
+def set_user_grp(file_path, user, group):
+    """ Set the UID and GUID of a file """
+    uid = pwd.getpwnam(user).pw_uid
+    gid = grp.getgrnam(group).gr_gid
+    os.chown(file_path, uid, gid)
+
+
+def delete_all_gifs(dir_path):
+    file_list = [f for f in os.listdir(dir_path) if f.endswith(".gif") or f.endswith(".png")]
+    for f in file_list:
+        file_del = os.path.join(dir_path, f)
+        os.remove(file_del)
+
+
+def parseargs(parse):
+    parse.add_argument('-d', '--dontdelete', action='store_true',
+                       help="Don't delete the images already in sync/")
+    return parse.parse_args()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Longer Weather")
+    args = parseargs(parser)
+    assure_path_exists(COMB_PATH)
+    assure_path_exists(FRAME_PATH)
+
+    logger.debug('Deleting gifs')
+    delete_all_gifs(FRAME_PATH)
+
+    longer_weather = LongerWeather()
+    longer_weather.start()
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'gs:n:h',
-            ["generate", "number", "sync", "help"])
-    except getopt.GetoptError as err:
-        print(err) # will print "option -a not recognized"
-        usage()
-        sys.exit(2)
-
-    for opt, arg in opts:
-        if opt in ("-g", "--generate"):
-            logging.info("Generating large GIF animation")
-            Combine_GIF()
-            logging.info("GIF generation complete")
-            sys.exit(0)
-        elif opt in ("-s", "--sync"):
-            if len(sys.argv) > 3:
-                image_number = int(float(sys.argv[3]))
-            daemon(int(float(sys.argv[2])))
-            sys.exit(0)
-        elif opt in ("-h", "--help"):
-            usage()
-            sys.exit(0)
-        else:
-            assert False, "Fail"
-            
-def daemon(sync_minutes):
-    global image_number
-    future = datetime.now()
-    
-    logging.info("[Daemon] Daemon Started")
-    
-    if image_number > 0:
-        logging.info("[Daemon] Starting image numbering at %s", image_number)
-    
-    # Synchronize to the correct time before beginning 2-hour interval timing
-    if sync_minutes:
-        future = future + timedelta(minutes = sync_minutes)
-        logging.info("[Daemon] Sync: wait %s minutes, Start at %s", sync_minutes, future)
-        while True:
-            present = datetime.now()
-            if present > future:
-                break
-            time.sleep(1.0)
-    
-    while True:
-        image_number += 1
-
-        #present = datetime.now()
-        future = future + timedelta(hours = 2, minutes = 15)
-        
-        logging.info("[Daemon] Grabbing GIF #%s from intellicast.net", image_number)
-        Get_GIF()
-        
-        logging.info("[Daemon] Combining individual GIFs into one large GIF animation")
-        Combine_GIF()
-        
-        logging.info("[Daemon] Next Grab: %s", future)
-
-        while True:
-            present = datetime.now()
-            if present > future:
-                break
-            time.sleep(1.0)
-    
-def Get_GIF():
-    date_now = datetime.now().strftime("%Y%m%d%H%M%S")
-    image_file = "%s/split/US-whole-%s-%s.gif" % (install_directory, date_now, image_number)
-    urllib.urlretrieve(
-        "http://images.intellicast.com/WxImages/RadarLoop/usa_None_anim.gif",
-        image_file)
-
-    logging.info("[Daemon] Changing last frame delay from 2.0 sec to 0.25 sec")
-    gif_slice = "gifsicle -b %s -d25 \"#8\"" % image_file
-    os.system(gif_slice)
-    
-def Combine_GIF():
-    date_now = datetime.now().strftime("%Y%m%d%H%M%S")
-    gifsicle = "gifsicle %s/split/*.gif > %s/combined/US-Combined-%s.gif" % (install_directory, install_directory, date_now)
-    os.system(gifsicle)
-    
-menu()
+        while longer_weather.is_alive():
+            time.sleep(1)
+    except KeyboardInterrupt:
+        logger.error("Keyboard exit")
+        longer_weather.terminate()
+        sys.exit(1)
